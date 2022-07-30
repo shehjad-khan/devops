4c5e71bfb2e136e1d3126a7b4363e0ec5d1f88ae
Shehjad Khan <shehjad.khan@ansys.com>
2022-07-30 13:36:29 -0500

    Initial code push

---
+++ mysite/ui.tests-java/src/test/java/com/ansysincprogram/selenium/LaunchBrowser.java
@@ -0,0 +1,23 @@
+package com.ansysincprogram.selenium;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
+public class LaunchBrowser {
+
+	@Test
+	public void myTest() throws MalformedURLException {
+		DesiredCapabilities dc = DesiredCapabilities.chrome();
+		URL url = new URL("http://localhost:4444/wd/hub");
+		RemoteWebDriver driver = new RemoteWebDriver(url, dc);
+		driver.get("https://www.ansys.com/");
+		String title = driver.getTitle();
+		System.out.println(title);
+		driver.quit();
+	}
+
+}
